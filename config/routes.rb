48c1dc473ac17d9a9aa9fe2d034acbed66f90d9f
mavenraven <developers@mavenraven.org>
2024-05-18 15:00:49 -0400

    Maps routes for forecasts controller to root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'forecasts/index'
-  get 'forecasts/show'
+  root 'forecasts#index'
+  get '/:id', to: 'forecasts#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
