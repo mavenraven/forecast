948644f1c7638ce09c5bdb7065bb3db0e8015094
mavenraven <developers@mavenraven.org>
2024-05-20 19:29:16 -0400

    Fixes routing to be idiomatic for root path.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root 'forecasts#index'
-  post '/', to: 'forecasts#create'
-  get '/:id', to: 'forecasts#show', as: :forecasts_show
+  resources :forecasts, path: ""
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
