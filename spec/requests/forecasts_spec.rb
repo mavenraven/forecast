87c2b3c4c24018948b8eb4b0670f9cc9e0485313
mavenraven <developers@mavenraven.org>
2024-05-20 13:23:20 -0400

    Adds failing test for show.

--- spec/requests/forecasts_spec.rb
+++ spec/requests/forecasts_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe "Forecasts", type: :request do
       expect(response.body).to include("Weather")
     end
   end
+
+  describe "GET show" do
+    it "retrieves the weather for a given zip code" do
+      get "/95014"
+      expect(response.body).to include("Cupertino")
+      expect(response.body).to include("75°")
+    end
+  end
 end
