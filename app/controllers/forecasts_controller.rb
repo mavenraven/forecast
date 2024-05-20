235c352fea9ea91892b3ad3af3ce20e3afa8859e
mavenraven <developers@mavenraven.org>
2024-05-20 15:26:42 -0400

    --wip-- [skip ci]

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,6 +3,11 @@ class ForecastsController < ApplicationController
   end
 
   def create
+    address = params[:address_input]
+    if address.match? /\A[\p{L}\p{N},.\s]+\z/u
+      redirect_to(forecasts_show_path(95014))
+    end
+    # Geocoder.coordinates(pa)
   end
 
   def show
---
+++ app/forms/address_form.rb
@@ -0,0 +1,8 @@
+class AddressForm
+  include ActiveModel::Model
+
+  attr_accessor :address
+
+  validates :address, presence: true,
+            format: {with: /\A[\p{L}\p{N},.\s]+\z/u, message: 'contains invalid characters'}
+end
